2f24e5ee032fc333755bff000e6eb0bdc25a4da2
ElenaZht <elenazht@gmail.com>
2021-07-18 23:04:29 +0300

    - sign up component complete.

--- server/server.js
+++ server/server.js
@@ -60,9 +60,9 @@ server.post('/signup', (req, res) => {
   const user = DB.get('users').find({email: email}).value();
   if (!user) {
     DB.get('users').push({email, password, role: "user"}).write();
-    res.sendStatus(200);
+    setTimeout(() => res.status(200).json({}), parseInt(Math.random()*3000+2000));
   } else {
-    res.status(404).json({msg: 'user already exists.'})
+    setTimeout(() => res.status(404).json({msg: 'user already exists.'}), parseInt(Math.random()*3000+2000));
   }
 });
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialogRef} from '@angular/material';
-import {UsersService} from '../users.service';
+import {User, UsersService} from '../users.service';
 import {NgForm} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-signup',
@@ -10,8 +12,9 @@ import {NgForm} from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   errorText: string;
+  user = {login: '', email: '', bday: '', password: '', phone: ''};
 
-  constructor(public dialogRef: MatDialogRef<SignupComponent>, private usersService: UsersService) {
+  constructor(public dialogRef: MatDialogRef<SignupComponent>, private usersService: UsersService, private toastr: ToastrService, private spinner: NgxSpinnerService) {
     this.errorText = '';
 
   }
@@ -22,6 +25,31 @@ export class SignupComponent implements OnInit {
     this.dialogRef.close();
   }
   onSubmit(SignupForm: NgForm) {
-    return true;
+    this.spinner.show();
+    const user = SignupForm.value as User;
+    console.log('user: ', user);
+    this.usersService.AddUser(user).subscribe(
+      () => {
+        this.spinner.hide();
+        this.exit();
+        console.log('new user! ', user);
+        this.showSuccess(user);
+      }, err => {
+        this.spinner.hide();
+        console.log('error from sign up ', err);
+        this.showError(user);
+
+      }
+      );
+
+    }
+  showSuccess(u) {
+    this.toastr.success('You signed up as ' + u.email, 'Signed up successfully!' );
   }
-}
+
+  showError(u) {
+    this.toastr.error('User with email: ' + u.email + ' is already exists. Please, try again.', 'Not signed up!');
+
+  }
+
+  }
